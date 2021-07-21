c42835851a5a3cdfcfd35aaeb9e59778d6f0cc08
Candice36 <candicebell@yahoo.com>
2021-07-20 22:20:30 -0500

    Add project files.

---
+++ Proj 6 Except Handling/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = Format(Val(TextBox1.Text), "0.000")
+
+    End Sub
+End Class
